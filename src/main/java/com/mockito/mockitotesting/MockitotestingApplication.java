dae44896ff9a8e3e0805dba1e3ad3db066ad32d9
Ilias Mertzanidis <ilias.mertzanidis@gus-group.com>
2020-01-20 11:19:50 +0100

    first commit

---
+++ src/main/java/com/mockito/mockitotesting/MockitotestingApplication.java
@@ -0,0 +1,13 @@
+package com.mockito.mockitotesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitotestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockitotestingApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/mockito/mockitotesting/Portofolio/interfaces/StockService.java
@@ -0,0 +1,7 @@
+package com.mockito.mockitotesting.Portofolio.interfaces;
+
+import com.mockito.mockitotesting.Portofolio.Stock;
+
+public interface StockService {
+    public double getPrice(Stock stock);
+}
