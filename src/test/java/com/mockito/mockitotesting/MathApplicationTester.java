c8bb46d9ca49e7d90c7cfb07e01a5df67beb431f
Ilias Mertzanidis <ilias.mertzanidis@gus-group.com>
2020-01-20 11:37:39 +0100

    testing Mockito.verify method to check invoke numbers of a mock method

--- src/test/java/com/mockito/mockitotesting/MathApplicationTester.java
+++ src/test/java/com/mockito/mockitotesting/MathApplicationTester.java
@@ -2,6 +2,8 @@ package com.mockito.mockitotesting;
 
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.never;
 
 import com.mockito.mockitotesting.mathApp.MathApplication;
 import com.mockito.mockitotesting.mathApp.interfaces.CalculatorService;
@@ -47,5 +49,31 @@ public class MathApplicationTester {
 
         //verify the behavior
         verify(calcService).add(20.0, 30.0);
+    }
+
+    @Test
+    public void testVerifyCallNumbers(){
+        //add the behavior of calc service to add two numbers
+        when(calcService.add(10.0,20.0)).thenReturn(30.00);
+
+        //add the behavior of calc service to subtract two numbers
+        when(calcService.subtract(20.0,10.0)).thenReturn(10.00);
+
+        //test the add functionality
+        Assert.assertEquals(mathApplication.add(10.0, 20.0),30.0,0);
+        Assert.assertEquals(mathApplication.add(10.0, 20.0),30.0,0);
+        Assert.assertEquals(mathApplication.add(10.0, 20.0),30.0,0);
+
+        //test the subtract functionality
+        Assert.assertEquals(mathApplication.subtract(20.0, 10.0),10.0,0.0);
+
+        //default call count is 1
+        verify(calcService).subtract(20.0, 10.0);
+
+        //check if add function is called three times
+        verify(calcService, times(3)).add(10.0, 20.0);
+
+        //verify that method was never called on a mock
+        verify(calcService, never()).multiply(10.0,20.0);
     }
 }
